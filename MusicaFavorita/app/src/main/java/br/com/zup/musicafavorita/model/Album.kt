f038df1704923bca59d79a78588f506264b28c3d
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-06-25 21:47:37 -0300

    Implementar data class model

--- MusicaFavorita/app/src/main/java/br/com/zup/musicafavorita/model/Album.kt
+++ MusicaFavorita/app/src/main/java/br/com/zup/musicafavorita/model/Album.kt
@@ -1,15 +1,3 @@
 package br.com.zup.musicafavorita.model
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-class Album (private val image:Int, private val name: String,
-             private val desc:String, private val info:String
-): Parcelable {
-    fun getImage() = this.image
-    fun getName() = this.name
-    fun getDesc() = this.desc
-    fun getInfo() = this.info
-}
-
+data class Album(var image: Int, var name: String, var description: String, var informations:String)
