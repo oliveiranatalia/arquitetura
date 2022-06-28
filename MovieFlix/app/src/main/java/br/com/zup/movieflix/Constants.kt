376c86137b28e38688b119aa2750174d2305d024
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-06-28 19:56:40 -0300

    Inserir aplicacao

---
+++ MovieFlix/app/src/main/java/br/com/zup/movieflix/Constants.kt
@@ -0,0 +1,17 @@
+package br.com.zup.movieflix
+
+const val SAVE_USER_PASS_FLAG_KEY = "br.com.zup.movieflix.SAVE_USER_PASS_FLAG_KEY"
+const val PREFERENCE_KEY = "br.com.zup.movieflix.PREFERENCE_KEY"
+const val USER_NAME_LOGIN_KEY = "br.com.zup.movieflix.USER_NAME_LOGIN_KEY"
+const val USER_PASSWORD_LOGIN_KEY = "br.com.zup.movieflix.USER_PASSWORD_LOGIN_KEY"
+
+const val KEY_MOVIE = "KEY_MOVIE"
+
+const val PREFERENCE_REGISTER_KEY = "br.com.zup.movieflix.PREFERENCE_REGISTER_KEY"
+const val USER_NAME_REGISTER_KEY = "br.com.zup.movieflix.USER_NAME_REGISTER_KEY"
+const val USER_PASSWORD_REGISTER_KEY = "br.com.zup.movieflix.USER_PASSWORD_REGISTER_KEY"
+const val USER_EMAIL_REGISTER_KEY = "br.com.zup.movieflix.USER_EMAIL_REGISTER_KEY"
+
+const val ERROR = "Usuario ou senha invalidos"
+const val REQUIRED = "Campo obrigatório"
+const val DMATCH = "As senhas devem ser iguais"
