9833d44a68a02539d250a1ef3137c404d4dfbaab
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-06-28 01:10:43 -0300

    Implementar logica repository, viewmodel e view

---
+++ MovieFlix/app/src/main/java/br/com/zup/movieflix/const/Consts.kt
@@ -0,0 +1,4 @@
+package br.com.zup.movieflix.const
+
+const val REQUIRED = "Campo obrigatório"
+const val RECORD = "Cadastro realizado com sucesso!"
--- MovieFlix/app/src/main/java/br/com/zup/movieflix/register/repository/RegisterRepository.kt
+++ MovieFlix/app/src/main/java/br/com/zup/movieflix/register/repository/RegisterRepository.kt
@@ -1,4 +1,15 @@
 package br.com.zup.movieflix.register.repository
 
+import br.com.zup.movieflix.register.model.RegisterModel
+
 class RegisterRepository {
+
+    fun verify(model: RegisterModel): RegisterModel? {
+        return when {
+            model.userName.isNotEmpty() &&
+                model.email.isNotEmpty() &&
+                    model.password.isNotEmpty() -> RegisterModel(model.userName,model.email,model.password)
+            else -> null
+        }
+    }
 }
