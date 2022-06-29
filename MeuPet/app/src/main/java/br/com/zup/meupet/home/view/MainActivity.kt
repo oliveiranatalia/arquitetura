0b9a0859d521ed3f03978cc0c45b45da234e9ea6
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-06-29 12:16:48 -0300

    Corrigir binding.root

--- MeuPet/app/src/main/java/br/com/zup/meupet/home/view/MainActivity.kt
+++ MeuPet/app/src/main/java/br/com/zup/meupet/home/view/MainActivity.kt
@@ -4,12 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import br.com.zup.meupet.R
 import br.com.zup.meupet.REQUIRED
 import br.com.zup.meupet.databinding.ActivityMainBinding
 import br.com.zup.meupet.home.model.MainModel
 import br.com.zup.meupet.home.viewmodel.MainViewModel
-import br.com.zup.meupet.pet.PetActivity
+import br.com.zup.meupet.pet.view.PetActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,29 +18,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        viewModel.getDataSaved()
-
-        viewModel.savedData.observe(this){
-            binding.etName.setText(it.name)
-        }
-        viewModel.savedDataFlag.observe(this){
-            binding.swSaveData.isChecked = it
-        }
         binding.btSaveName.setOnClickListener{
             registration()
         }
+        viewModel.getDataSaved()
+        observers()
     }
     fun registration(){
-        val petName = binding.etName.text.toString()
-        if(petName.isEmpty() || petName.isBlank()){
-            binding.etName.error = REQUIRED
-        }else{
-            val pet = MainModel(petName)
-            viewModel.authentication(pet, binding.swSaveData.isChecked)
-            startActivity(Intent(this, PetActivity::class.java))
-        }
-    }
+       val petName = binding.etName.text.toString()
+       if(petName.isEmpty() || petName.isBlank()){
+           binding.etName.error = REQUIRED
+       }else{
+           val pet = MainModel(petName)
+           viewModel.authentication(pet, binding.swSaveData.isChecked)
+           startActivity(Intent(this, PetActivity::class.java))
+       }
+   }
+   fun observers(){
+       viewModel.savedData.observe(this){
+           binding.etName.setText(it.name)
+       }
+       viewModel.savedDataFlag.observe(this){
+           binding.swSaveData.isChecked = it
+       }
+   }
 }
