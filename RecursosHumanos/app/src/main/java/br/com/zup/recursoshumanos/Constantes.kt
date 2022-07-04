12a615b9134a37756f0bca9b4a38b1de8482f786
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-04 12:01:22 -0300

    Inserir aplicacao

---
+++ RecursosHumanos/app/src/main/java/br/com/zup/recursoshumanos/Constantes.kt
@@ -0,0 +1,7 @@
+package br.com.zup.recursoshumanos
+
+const val KEY = "funcionario"
+const val ERROR = "Campo obrigatório!"
+const val HORA = "Horas trabalhadas: "
+const val VAL = "Valor por hora: R$ "
+const val SAL = "Salário a receber: R$ "
