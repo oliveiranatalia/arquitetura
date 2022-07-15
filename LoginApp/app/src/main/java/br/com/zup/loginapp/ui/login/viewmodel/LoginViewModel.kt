e67c2b50e45c2f4059f9cc0b3be286940cd249db
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-15 00:42:40 -0300

    Implementar login

---
+++ LoginApp/app/src/main/java/br/com/zup/loginapp/ui/login/viewmodel/LoginViewModel.kt
@@ -0,0 +1,35 @@
+package br.com.zup.loginapp.ui.login.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.com.zup.loginapp.constants.ERROR
+import br.com.zup.loginapp.constants.REQUIRED
+import br.com.zup.loginapp.domain.model.User
+import br.com.zup.loginapp.domain.repository.AuthenticatonRepository
+
+class LoginViewModel: ViewModel() {
+    private val repository = AuthenticatonRepository()
+
+    private var _login = MutableLiveData<User>()
+    var login: LiveData<User> = _login
+
+    private var _error = MutableLiveData<String>()
+    var error: LiveData<String> = _error
+
+    fun validate(user:User){
+        when{
+            user.email.isEmpty() ->_error.value = REQUIRED
+            user.password.isEmpty() -> _error.value = REQUIRED
+            else -> login(user)
+        }
+    }
+    fun login(user:User){
+        try{
+            repository.login(user.email,user.password)
+            _login.value = user
+        }catch(e:Exception){
+            _error.value = ERROR
+        }
+    }
+}
